d8ec77e3bc2aab9850ac1f00bfc6a939da0125f4
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2024-12-11 21:33:21 +0700

    Add files via upload

---
+++ Modul 5/16-Super()/src/com/tutorial/Main.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+public class Main {
+    public {
+        //membuat objek dari superclass
+        Hero hero1 = new Hero();
+        //membuat objek dari subclass
+        HeroTank hero2 = new HeroTank();
+        hero1.cetak();
+        hero2.cetak();
+    }
+    
+}
---
+++ Modul 5/17-Subclass Construktor/src/com/tutorial/Main.java
@@ -0,0 +1,8 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        HeroTank hero1 = new HeroTank("lance", 98, 120);
+        hero1.cetak(); 
+    }
+}
